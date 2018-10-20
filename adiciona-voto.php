1c1068db8b7aa00e064884b6368119fc275d02e7
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2018-10-20 14:09:22 -0300

    Comunicação com a API
    Realizando a comunicação com a API através do CURL, com JSON e o POST.

--- adiciona-voto.php
+++ adiciona-voto.php
@@ -4,30 +4,29 @@
 	require_once("banco-votos.php");
 	require_once("usuario-session.php");
 
+	header("Access-Control-Allow-Origin: *");
+
 	verificaUsuario();
 
 	$id_usuario = $_POST["id_usuario"];
 	$id_eleicao = $_POST["id_eleicao"];
 	$id_candidato_eleicao = $_POST["id_candidato_eleicao"];
 
-	if(insereUsuarioEleicao($conexao, $id_usuario, $id_eleicao) && insereVoto($conexao, $id_candidato_eleicao)) { ?>
-		<script>
-			$(document).ready(function() {
-				var dadoJson = '{ "dado": "' + <?=$id_candidato_eleicao?> + '" }';
-				console.log(dadoJson);
+	if(insereUsuarioEleicao($conexao, $id_usuario, $id_eleicao) && insereVoto($conexao, $id_candidato_eleicao)) { 
+		$api_url = 'http://localhost:8000/mine';
+		$ch = curl_init($api_url);
+		
+		$json = array(
+			'dado' => "{$id_candidato_eleicao}"
+		);
+		
+		$jsonEncodado = json_encode($json);
 
-				/*$.ajax({
-					method: "POST",
-					url: "https://whispering-refuge-53254.herokuapp.com/mine",
-					data: dadoJson,
-					dataType: "json"
-				}).done(function(msg) {
-					alert("Data Saved: " + msg);
-				});*/
-
-			});
-		</script>
-		<p class="alert alert-success">Voto foi cadastrado com sucesso!</p> <?php 
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonEncodado);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); ?>
+		<p class="alert alert-success">Voto foi cadastrado com sucesso!</p>
+		<p class="d-none"><?=curl_exec($ch);?></p> <?php 
     } else {
     	$msg = mysqli_error($conexao); ?>
     	<p class="alert alert-danger">Erro ao cadastrar voto: <?= $msg ?></p> <?php
